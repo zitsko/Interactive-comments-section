52e1a9336836bfce774aabd7e02be84c298b3a98
JasonGal <jason.gal.bu@gmail.com>
2023-07-09 16:21:24 +0300

    comments.js &  added replies functionality

--- client/src/Components/Comments.js
+++ client/src/Components/Comments.js
@@ -9,12 +9,16 @@ function CommentSection() {
     userId: '',
     createdAt: Date.now(),
     upvotes: '',
+    replies: [],
   });
   const [comments, setComments] = useState([]);
   const [user, setUser] = useState({
     _id: '',
     email: '',
   });
+  const [replyContent, setReplyContent] = useState('');
+
+  
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -29,7 +33,7 @@ function CommentSection() {
             axios
               .get('http://localhost:3005/comment/' + data.userData._id)
               .then(({ data }) => {
-                console.log("user comments", data);
+                console.log('user comments', data);
                 setComments(data);
               });
           } else {
@@ -53,7 +57,7 @@ function CommentSection() {
   const handleCommentSubmit = (event) => {
     event.preventDefault();
     console.log(newComment);
-  
+
     axios
       .post('http://localhost:3005/comment', newComment)
       .then(() => {
@@ -62,6 +66,7 @@ function CommentSection() {
           userId: '',
           createdAt: Date.now(),
           upvotes: '',
+          replies: [],
         });
         // Fetch the updated comments after successful submission
         axios
@@ -83,12 +88,14 @@ function CommentSection() {
     axios
       .delete(`http://localhost:3005/comment/${commentId}`)
       .then(() => {
-        setComments((prevComments) => prevComments.filter((comment) => comment._id !== commentId));
+        setComments((prevComments) =>
+          prevComments.filter((comment) => comment._id !== commentId)
+        );
       })
       .catch((error) => {
         console.error('Error deleting comment:', error);
       });
-  };
+  }
 
   const handleCommentEdit = (commentId, updatedContent) => {
     axios
@@ -110,35 +117,99 @@ function CommentSection() {
       });
   };
 
+  const handleReply = (commentId) => {
+    const reply = {
+      content: replyContent,
+      userId: user._id,
+      createdAt: Date.now(),
+    };
+
+    axios
+      .post(`http://localhost:3005/comment/${commentId}/reply`, reply)
+      .then(() => {
+        // Fetch the updated comments after successful reply
+        axios
+          .get('http://localhost:3005/comment/' + user._id)
+          .then(({ data }) => {
+            console.log('user comments', data);
+            setComments(data);
+          })
+          .catch((error) => {
+            console.error('Error fetching comments:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error sending reply data:', error);
+      });
+  };
+
   return (
     <div>
       <h2>Comments</h2>
       <form onSubmit={handleCommentSubmit}>
         <label>
           Comment:
-          <textarea
-          name="content"
-          value={newComment.content}
-          onChange={handleCommentChange}
-        />
+          <textarea name="content" value={newComment.content} onChange={handleCommentChange} />
         </label>
         <br />
         <button type="submit">Submit</button>
       </form>
       <div className="comment-section">
-        {comments.map((newComment) => (
-          <div key={newComment._id} className="comment-item">
-            <p>Content: {newComment.content}</p>
-            <p>UserID: {newComment.userId}</p>
-            <p>Created At: {newComment.createdAt}</p>
-            <p>Upvotes: {newComment.upvotes}</p>
-            <button onClick={() => handleCommentEdit(newComment._id, 'Updated Content')}>Edit</button>
-            <button onClick={() => handleCommentDelete(newComment._id)}>Delete</button>
-           </div>
-         ))}
-      </div>
-      </div>
-  );
-}
+        {comments.map((comment) => (
+          <div key={comment._id} className="comment-item">
+            <p>Content: {comment.content}</p>
+            <p>UserID: {comment.userId}</p>
+            <p>Created At: {comment.createdAt}</p>
+            <p>Upvotes: {comment.upvotes}</p>
+            <button onClick={() => handleCommentEdit(comment._id, 'Updated Content')}>
+              Edit
+            </button>
+            <button onClick={() => handleCommentDelete(comment._id)}>Delete</button>
 
-export default CommentSection;
+            {/* Render Replies */}
+            <div className="replies">
+              {comment.replies &&
+                comment.replies.map((reply) => (
+                  <div key={reply._id} className="reply-item">
+                    <p>Content: {reply.content}</p>
+                    <p>UserID: {reply.userId}</p>
+                    <p>Created At: {reply.createdAt}</p>
+
+                    {/* Render Nested Replies */}
+                    {reply.replies &&
+                      reply.replies.map((nestedReply) => (
+                        <div key={nestedReply._id} className="nested-reply-item">
+                          <p>Content: {nestedReply.content}</p>
+                          <p>UserID: {nestedReply.userId}</p>
+                          <p>Created At: {nestedReply.createdAt}</p>
+                        </div>
+                      ))}
+                  </div>
+                ))}
+            </div>
+            {/* Reply Form */}
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                handleReply(comment._id);
+                setReplyContent('');
+              }}
+            >
+              <label>
+                Reply:
+                <textarea
+                  name="replyContent"
+                  value={replyContent}
+                  onChange={(event) => setReplyContent(event.target.value)}
+                  />
+                </label>
+                <button type="submit">Submit Reply</button>
+              </form>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default CommentSection;
